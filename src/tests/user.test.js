344df60515a32dc360b6b4c8ac56787b0bd875be
Alejandro Villavicencio <alejandro.viig@gmail.com>
2024-02-20 21:00:05 -0600

    secure endpoint test

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -4,6 +4,8 @@ const User = require('../models/User');
 
 const BASE_URL = "/users";
 
+let token;
+
 const user = {
     firstName: "Victor",
     lastName: "Villa",
@@ -11,6 +13,29 @@ const user = {
     password: "victor1234",
     phone: "+5211223344"
 }
+
+beforeAll(async () => {
+    const user = {
+        email: "fernando@gmail.com",
+        password: "fernando1234"
+    }
+
+    const res = await request(app).post(`${BASE_URL}/login`)
+                                  .send(user);
+
+    token = res.body.token;
+});
+
+test("GET -> /users, should return status code 200, res.body is defined, and res.body.length === 1",
+    async () => {
+        const res = await request(app).get(BASE_URL)
+                                      .set('authorization', `Bearer ${token}`);
+
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toBeDefined();
+        expect(res.body).toHaveLength(1);
+    }
+);
 
 test("POST -> /users, should return status code 201, res.body is defined, and res.body.firstName === user.firstName",
     async () => {
